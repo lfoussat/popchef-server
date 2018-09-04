2db17f452cf31c7d868af662b7c9dfdf79846081
Louise Foussat <louisefoussat@gmail.com>
2018-09-04 13:25:09 +0200

    connect db

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express()
 const port = 4000
 
 const mysql = require('mysql2/promise')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'popchef'
+})
+
+const exec = async (query, params) => {
+  const connect = await connection
+  const result = await connect.execute(query, params)
+
+  return result[0]
+}
 app.get('/', (req, res) => {
   res.send('OK')
 })
