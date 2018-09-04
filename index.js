eb1ee952f91ad917889f64641eb9af7a48aec75c
Louise Foussat <louisefoussat@gmail.com>
2018-09-04 12:09:04 +0200

    first route test

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.get('/', (req, res) => {
+  res.send('OK')
+})
+
 app.listen(port, err => console.log(err || `server listening on port ${port}`))
