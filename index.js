79ee050ea08ea92fb43ce437c728b7605ef7373a
Louise Foussat <louisefoussat@gmail.com>
2018-09-04 13:26:24 +0200

    enable cross-origin resource sharing

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const exec = async (query, params) => {
 
   return result[0]
 }
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 app.get('/', (req, res) => {
   res.send('OK')
 })
